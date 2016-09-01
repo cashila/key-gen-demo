cdd55ce0fbc66a435ed078af7082cea72ecd1cb9
alesl <aleslekse@gmail.com>
2016-09-01 23:11:29 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Install & run
+
+```
+npm install
+npm start
+```
+
+# Caveats
+
+- Ether, when address is hashed, sha3 is used, but this is not standard version, but the one used in proposal (afaik same params differ)
+- For ethereum it would be best, if Mist compatible wallet would be generated because it would be easiest to use, not sure what to do about password with which wallet should be encrypted
+- For Lisk, it would be best if device generated seed, because this is used primary lisk wallet software
---
+++ index.js
@@ -0,0 +1,68 @@
+const chalk = require('chalk');
+
+//
+// Ether
+//
+// https://github.com/ethereumjs/ethereumjs-util/blob/master/index.js
+//
+const ethUtil = require('ethereumjs-util');
+const crypto = require('crypto');
+const EthWallet = require('ethereumjs-wallet');
+var buffer;
+
+function genEther(buffer) {
+  var privateKey = buffer;
+  var publicKey = ethUtil.privateToPublic(privateKey);
+  var address = ethUtil.publicToAddress(publicKey);
+  var wallet = EthWallet.fromPrivateKey(privateKey).toV3String('test');
+
+  console.log(`Private key: ${privateKey.toString('hex')}`)
+  console.log(`Public key: ${publicKey.toString('hex')}`)
+  console.log(`Address: ${chalk.green( '0x'+address.toString('hex') )}`)
+  console.log(`Mist wallet: ${chalk.grey(wallet)}`);
+  console.log();
+}
+
+
+console.log(`ETHER (random):`);
+buffer = crypto.randomBytes(32); // random 32 bytes
+genEther(buffer);
+
+console.log(`ETHER (expected address ${chalk.blue('0x064eccdf9e1e40d8a3acf784cf6df9e862f71469')}):`);
+buffer = new Buffer('f274eff1d47d5a9c72f8874f227937ccd937a876bf7815847f76ff7521dfb063', 'hex');
+genEther(buffer);
+
+//
+// Lisk
+//
+// https://github.com/LiskHQ/lisk-js/blob/development/lib/transactions/crypto.js
+//
+const Nacl = require("js-nacl");
+const lisk = require('lisk-js');
+
+function genLisk(nacl, buffer) {
+  var keyPair = nacl.crypto_sign_keypair_from_seed(buffer);
+  var privateKey = new Buffer(keyPair.signSk);
+  var publicKey = new Buffer(keyPair.signPk);
+  var address = lisk.crypto.getAddress(publicKey);
+
+  console.log(`Private key: ${privateKey.toString('hex')}`);
+  console.log(`Public key: ${publicKey.toString('hex')}`);
+  console.log(`Address: ${chalk.green( address )}`);
+  console.log();
+}
+
+Nacl.instantiate(function(nacl) {
+  var buffer, seed;
+
+  console.log('LISK (random)');
+  buffer = crypto.randomBytes(32);
+  genLisk(nacl, buffer);
+
+  console.log(`LISK (expected address ${chalk.blue('12137408304683933905L')})`);
+  console.log(`Seed: ${seed}`);
+  seed = 'uncle latin swing toilet reform mimic neglect engine brave isolate spatial useless';
+  buffer = crypto.createHash("sha256").update(seed, "utf8").digest();
+  genLisk(nacl, buffer);
+});
+
